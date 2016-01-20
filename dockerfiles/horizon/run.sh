6db84cddd0a9575b19d47d569a01869137d781fd
osu <osu@api220.lab.local>
2016-01-19 19:33:22 -0500

    delete apache pid file before restarting

--- dockerfiles/horizon/run.sh
+++ dockerfiles/horizon/run.sh
@@ -1,6 +1,7 @@
 #! /bin/bash +ex
 memcached -u memcache &
+rm -f /var/run/apache2/apache2.pid
 /usr/sbin/apache2ctl -D FOREGROUND
-touch /var/log/apache2/access.log
-tail -F /var/log/apache2/access.log
+#touch /var/log/apache2/access.log
+#tail -F /var/log/apache2/access.log
 
