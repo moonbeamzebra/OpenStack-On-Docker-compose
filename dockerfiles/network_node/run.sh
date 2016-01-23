917e79166894f7929d8a37fb1481fc5a352ed5dd
jplaberge <moonbeamzebra@magenta.ca>
2016-01-23 11:54:57 -0500

    add network_node in container

---
+++ dockerfiles/network_node/run.sh
@@ -0,0 +1,29 @@
+#! /bin/bash +ex
+
+touch /var/log/openvswitch/ovs-ctl.log
+touch /var/log/openvswitch/ovsdb-server.log
+touch /var/log/openvswitch/ovs-vswitchd.log
+
+touch /var/log/neutron/dhcp-agent.log
+touch /var/log/neutron/ovs-cleanup.log
+touch /var/log/neutron/neutron-metadata-agent.log
+touch /var/log/neutron/openvswitch-agent.log
+touch /var/log/neutron/l3-agent.log
+
+service openvswitch-switch restart
+service neutron-plugin-openvswitch-agent restart
+service neutron-l3-agent restart
+service neutron-dhcp-agent restart
+service neutron-metadata-agent restart
+
+tail -v -F /var/log/openvswitch/ovs-ctl.log &
+tail -v -F /var/log/openvswitch/ovsdb-server.log &
+tail -v -F /var/log/openvswitch/ovs-vswitchd.log &
+
+tail -v -F /var/log/neutron/dhcp-agent.log &
+tail -v -F /var/log/neutron/ovs-cleanup.log &
+tail -v -F /var/log/neutron/neutron-metadata-agent.log &
+tail -v -F /var/log/neutron/openvswitch-agent.log &
+
+## Blocking one
+tail -v -F /var/log/neutron/l3-agent.log
