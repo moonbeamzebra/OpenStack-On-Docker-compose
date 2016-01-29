b37eef100ba5ab2fbd3e077c895d66786222fca8
moonbeamzebra <moonbeamzebra@magenta.ca>
2016-01-29 06:27:11 -0500

    support compute node in docker

---
+++ net-and-cmp-yaml/dockerfiles/compute_node/setup.sh
@@ -0,0 +1,173 @@
+#! /bin/bash
+
+if [ -f /setup.done ];
+then
+   echo "Setup done" > /tmp/done
+   exit 0
+fi
+
+cat <<EOF > /admin-openrc.sh
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=$ADMIN_PASS
+export OS_AUTH_URL=http://$KEYSTONE_HOST:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+EOF
+
+cat <<EOF > /demo-openrc.sh
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=demo
+export OS_TENANT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=$DEMO_PASS
+export OS_AUTH_URL=http://$KEYSTONE_HOST:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+EOF
+
+
+./wait_for_rabbitmq.sh
+
+
+service neutron-plugin-openvswitch-agent stop
+service neutron-l3-agent stop
+service neutron-dhcp-agent stop
+service neutron-metadata-agent stop
+
+cp /etc/sysctl.conf /etc/sysctl.conf.bak
+cat <<EOF >> /etc/sysctl.conf
+net.ipv4.ip_forward=1
+net.ipv4.conf.default.rp_filter=0
+net.ipv4.conf.all.rp_filter=0
+EOF
+sysctl -p
+
+cp /etc/neutron/neutron.conf /etc/neutron/neutron.conf.bak
+
+crudini --set /etc/neutron/neutron.conf DEFAULT core_plugin ml2
+crudini --set /etc/neutron/neutron.conf DEFAULT service_plugins router
+crudini --set /etc/neutron/neutron.conf DEFAULT allow_overlapping_ips True
+
+crudini --set /etc/neutron/neutron.conf DEFAULT rpc_backend rabbit
+
+crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_host $RABBIT_HOST
+crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_userid $RABBITMQ_DEFAULT_USER
+crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_password $RABBITMQ_DEFAULT_PASS
+
+crudini --set /etc/neutron/neutron.conf DEFAULT auth_strategy keystone
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_uri http://$KEYSTONE_HOST:5000
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_url http://$KEYSTONE_HOST:35357
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_plugin password
+crudini --set /etc/neutron/neutron.conf keystone_authtoken project_domain_id default
+crudini --set /etc/neutron/neutron.conf keystone_authtoken user_domain_id default
+crudini --set /etc/neutron/neutron.conf keystone_authtoken project_name service
+crudini --set /etc/neutron/neutron.conf keystone_authtoken username neutron
+crudini --set /etc/neutron/neutron.conf keystone_authtoken password $NEUTRON_PASS
+
+crudini --set /etc/neutron/neutron.conf DEFAULT notify_nova_on_port_status_changes True
+crudini --set /etc/neutron/neutron.conf DEFAULT notify_nova_on_port_data_changes True
+crudini --set /etc/neutron/neutron.conf DEFAULT nova_url http://$NOVA_HOST:8774/v2
+
+crudini --set /etc/neutron/neutron.conf nova auth_url http://$KEYSTONE_HOST:35357
+crudini --set /etc/neutron/neutron.conf nova auth_plugin password
+crudini --set /etc/neutron/neutron.conf nova project_domain_id default
+crudini --set /etc/neutron/neutron.conf nova user_domain_id default
+crudini --set /etc/neutron/neutron.conf nova region_name $REGION1
+crudini --set /etc/neutron/neutron.conf nova project_name service
+crudini --set /etc/neutron/neutron.conf nova username nova
+crudini --set /etc/neutron/neutron.conf nova password $NOVA_PASS
+
+crudini --set /etc/neutron/neutron.conf DEFAULT verbose True
+
+diff /etc/neutron/neutron.conf /etc/neutron/neutron.conf.bak
+
+sleep 5
+
+cp /etc/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugins/ml2/ml2_conf.ini.bak
+#crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 type_drivers flat,vlan,vxlan
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 type_drivers flat,vxlan
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 tenant_network_types vxlan
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 mechanism_drivers openvswitch,l2population
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 extension_drivers port_security
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_flat flat_networks external
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_vxlan vni_ranges 1:1000
+
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup enable_security_group True
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup enable_ipset True
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup firewall_driver neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver
+
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ovs local_ip $NET_OVERLAY_INTERFACE_IP_ADDRESS
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ovs enable_tunneling  True
+#crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ovs bridge_mappings vlan:br-vlan,external:br-ex
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ovs bridge_mappings external:br-ex
+
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini agent l2_population True
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini agent tunnel_types vxlan
+
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup firewall_driver neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup enable_security_group True
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup enable_ipset True
+
+
+diff /etc/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugins/ml2/ml2_conf.ini.bak
+
+
+sleep 5
+
+cp /etc/neutron/l3_agent.ini /etc/neutron/l3_agent.ini.bak
+crudini --set /etc/neutron/l3_agent.ini DEFAULT interface_driver neutron.agent.linux.interface.OVSInterfaceDriver
+crudini --set /etc/neutron/l3_agent.ini DEFAULT external_network_bridge ""
+crudini --set /etc/neutron/l3_agent.ini DEFAULT use_namespaces True
+crudini --set /etc/neutron/l3_agent.ini DEFAULT router_delete_namespaces True
+crudini --set /etc/neutron/l3_agent.ini DEFAULT verbose True
+diff /etc/neutron/l3_agent.ini /etc/neutron/l3_agent.ini.bak
+
+
+
+cp /etc/neutron/dhcp_agent.ini  /etc/neutron/dhcp_agent.ini.bak
+crudini --set /etc/neutron/dhcp_agent.ini DEFAULT interface_driver neutron.agent.linux.interface.OVSInterfaceDriver
+crudini --set /etc/neutron/dhcp_agent.ini DEFAULT dhcp_driver neutron.agent.linux.dhcp.Dnsmasq
+crudini --set /etc/neutron/dhcp_agent.ini DEFAULT enable_isolated_metadata True
+crudini --set /etc/neutron/dhcp_agent.ini DEFAULT use_namespaces True
+crudini --set /etc/neutron/dhcp_agent.ini DEFAULT dhcp_delete_namespaces True
+crudini --set /etc/neutron/dhcp_agent.ini DEFAULT verbose True
+crudini --set /etc/neutron/dhcp_agent.ini DEFAULT dnsmasq_config_file /etc/neutron/dnsmasq-neutron.conf
+diff /etc/neutron/dhcp_agent.ini  /etc/neutron/dhcp_agent.ini.bak
+
+echo "dhcp-option-force=26,1450" >> /etc/neutron/dnsmasq-neutron.conf
+
+sleep 5
+
+cp /etc/neutron/metadata_agent.ini /etc/neutron/metadata_agent.ini.bak
+crudini --set /etc/neutron/metadata_agent.ini DEFAULT auth_uri http://$KEYSTONE_HOST:5000
+crudini --set /etc/neutron/metadata_agent.ini DEFAULT auth_url http://$KEYSTONE_HOST:35357
+crudini --set /etc/neutron/metadata_agent.ini DEFAULT auth_region $REGION1
+crudini --set /etc/neutron/metadata_agent.ini DEFAULT auth_plugin password
+crudini --set /etc/neutron/metadata_agent.ini DEFAULT project_domain_id default
+crudini --set /etc/neutron/metadata_agent.ini DEFAULT user_domain_id default
+crudini --set /etc/neutron/metadata_agent.ini DEFAULT project_name service
+crudini --set /etc/neutron/metadata_agent.ini DEFAULT username neutron
+crudini --set /etc/neutron/metadata_agent.ini DEFAULT password $NEUTRON_PASS
+
+crudini --set /etc/neutron/metadata_agent.ini DEFAULT nova_metadata_ip $NOVA_HOST
+crudini --set /etc/neutron/metadata_agent.ini DEFAULT metadata_proxy_shared_secret $METADATA_SECRET
+crudini --set /etc/neutron/metadata_agent.ini DEFAULT verbose True
+
+diff /etc/neutron/metadata_agent.ini /etc/neutron/metadata_agent.ini.bak
+
+service openvswitch-switch restart
+ovs-vsctl add-br br-ex
+ovs-vsctl add-port br-ex $NET_PUBLIC_INTERFACE_NAME
+service openvswitch-switch stop
+
+#service neutron-plugin-openvswitch-agent restart
+#service neutron-l3-agent restart
+#service neutron-dhcp-agent restart
+#service neutron-metadata-agent restart
+
+touch /setup.done
---
+++ net-and-cmp-yaml/dockerfiles/compute_node/wait_for_mysql.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+((count = $WAIT_LOOPS))
+while [[ $count -ne 0 ]] ; do
+    sleep $WAIT_SLEEP
+    echo "show databases;" | mysql --user=root --password=$MYSQL_ROOT_PASSWORD -h $MYSQLHOST -P 3306
+    rc=$?
+    if [[ $rc -eq 0 ]] ; then
+        echo "MYSQL is responding OK"
+        ((count = 1))
+    else
+        echo "MYSQL is NOT responding yet"
+    fi
+    ((count = count - 1))
+done
+
+exit $rc
+
