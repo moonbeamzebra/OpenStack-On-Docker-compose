ff68be8f70f62f27f41b4de733d6e624274e8b92
jplaberge <moonbeamzebra@magenta.ca>
2016-02-07 14:15:04 -0500

    enhance heat test

--- launch-1st-instances/heat_hot_based/setup_private_network_with_heat.sh
+++ launch-1st-instances/heat_hot_based/setup_private_network_with_heat.sh
@@ -2,5 +2,5 @@ source ~/demo-openrc.sh
 
 PUB_NET_ID=$(neutron net-show -f value -F id public)
 
-heat stack-create -f test-stack.yml \
--P "public_network=$PUB_NET_ID" testStack
+heat stack-create -f private_net.yml \
+-P "public_network=$PUB_NET_ID" demo_private_net
