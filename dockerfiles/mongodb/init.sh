8696281ee736579fd15d64ec1386e90895d587af
jplaberge <moonbeamzebra@magenta.ca>
2016-06-12 09:23:55 -0400

    not required anymore

--- dockerfiles/mongodb/init.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-echo "$CEIL_PASS"
-mongo --host $MONGO_HOST --eval 'db = db.getSiblingDB("ceilometer");db.addUser({user: "ceilometer",pwd: "$CEIL_PASS",roles: [ "readWrite", "dbAdmin" ]})'
-
-
