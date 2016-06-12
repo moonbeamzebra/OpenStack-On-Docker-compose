c16345a7edaad73004c6f10bef74beb66e8f9f73
jplaberge <moonbeamzebra@magenta.ca>
2016-06-12 09:30:50 -0400

    new mitaka instructions

--- dockerfiles/keystone/setup.sh
+++ dockerfiles/keystone/setup.sh
@@ -18,15 +18,17 @@ cp /etc/keystone/keystone.conf /etc/keystone/keystone.conf.bak
 crudini --set /etc/keystone/keystone.conf DEFAULT admin_token $ADMIN_TOKEN
 crudini --set /etc/keystone/keystone.conf DEFAULT verbose True
 crudini --set /etc/keystone/keystone.conf database connection mysql+pymysql://keystone:$KEYSTONE_DBPASS@$MYSQLHOST/keystone
-crudini --set /etc/keystone/keystone.conf token provider uuid
-crudini --set /etc/keystone/keystone.conf token driver memcache
-crudini --set /etc/keystone/keystone.conf revoke driver sql
-crudini --set /etc/keystone/keystone.conf memcache servers localhost:11211
+crudini --set /etc/keystone/keystone.conf token provider fernet
+#crudini --set /etc/keystone/keystone.conf token driver memcache
+#crudini --set /etc/keystone/keystone.conf revoke driver sql
+#crudini --set /etc/keystone/keystone.conf memcache servers localhost:11211
 diff /etc/keystone/keystone.conf /etc/keystone/keystone.conf.bak
 
 sleep 5
 
-su -s /bin/sh -c "keystone-manage db_sync" keystone 2>&1 > /var/log/keystone/keystone-manage.out 
+su -s /bin/sh -c "keystone-manage db_sync" keystone 2>&1 > /var/log/keystone/keystone-manage.out
+
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
 
 cat <<EOF > /etc/apache2/sites-available/wsgi-keystone.conf
 Listen 5000
@@ -38,20 +40,12 @@ Listen 35357
     WSGIScriptAlias / /usr/bin/keystone-wsgi-public
     WSGIApplicationGroup %{GLOBAL}
     WSGIPassAuthorization On
-    <IfVersion >= 2.4>
-      ErrorLogFormat "%{cu}t %M"
-    </IfVersion>
+    ErrorLogFormat "%{cu}t %M"
     ErrorLog /var/log/apache2/keystone.log
     CustomLog /var/log/apache2/keystone_access.log combined
 
     <Directory /usr/bin>
-        <IfVersion >= 2.4>
-            Require all granted
-        </IfVersion>
-        <IfVersion < 2.4>
-            Order allow,deny
-            Allow from all
-        </IfVersion>
+        Require all granted
     </Directory>
 </VirtualHost>
 
@@ -61,20 +55,12 @@ Listen 35357
     WSGIScriptAlias / /usr/bin/keystone-wsgi-admin
     WSGIApplicationGroup %{GLOBAL}
     WSGIPassAuthorization On
-    <IfVersion >= 2.4>
-      ErrorLogFormat "%{cu}t %M"
-    </IfVersion>
+    ErrorLogFormat "%{cu}t %M"
     ErrorLog /var/log/apache2/keystone.log
     CustomLog /var/log/apache2/keystone_access.log combined
 
     <Directory /usr/bin>
-        <IfVersion >= 2.4>
-            Require all granted
-        </IfVersion>
-        <IfVersion < 2.4>
-            Order allow,deny
-            Allow from all
-        </IfVersion>
+        Require all granted
     </Directory>
 </VirtualHost>
 EOF
@@ -92,13 +78,15 @@ export OS_IDENTITY_API_VERSION=3
 
 openstack service create \
   --name keystone --description "OpenStack Identity" identity
-  
+
 openstack endpoint create --region $REGION1 \
-  identity public http://$KEYSTONE_HOST:5000/v2.0
+  identity public http://$KEYSTONE_HOST:5000/v3
 openstack endpoint create --region $REGION1 \
-  identity internal http://$KEYSTONE_HOST:5000/v2.0   
+  identity internal http://$KEYSTONE_HOST:5000/v3
 openstack endpoint create --region $REGION1 \
-  identity admin http://$KEYSTONE_HOST:35357/v2.0   
+  identity admin http://$KEYSTONE_HOST:35357/v3
+
+openstack domain create --description "Default Domain" default
 openstack project create --domain default \
   --description "Admin Project" admin
 openstack user create  --domain default --password $ADMIN_PASS admin
@@ -113,11 +101,11 @@ openstack role create user
 openstack role add --project demo --user demo user
 
 
-cp /etc/keystone/keystone-paste.ini /etc/keystone/keystone-paste.ini.bak
-crudini --del /etc/keystone/keystone-paste.ini pipeline:public_api admin_token_auth
-crudini --del /etc/keystone/keystone-paste.ini pipeline:admin_api admin_token_auth
-crudini --del /etc/keystone/keystone-paste.ini pipeline:api_v3 admin_token_auth
-diff /etc/keystone/keystone-paste.ini /etc/keystone/keystone-paste.ini.bak
+#cp /etc/keystone/keystone-paste.ini /etc/keystone/keystone-paste.ini.bak
+#crudini --del /etc/keystone/keystone-paste.ini pipeline:public_api admin_token_auth
+#crudini --del /etc/keystone/keystone-paste.ini pipeline:admin_api admin_token_auth
+#crudini --del /etc/keystone/keystone-paste.ini pipeline:api_v3 admin_token_auth
+#diff /etc/keystone/keystone-paste.ini /etc/keystone/keystone-paste.ini.bak
 
 cat <<EOF > /admin-openrc.sh
 export OS_PROJECT_DOMAIN_ID=default
