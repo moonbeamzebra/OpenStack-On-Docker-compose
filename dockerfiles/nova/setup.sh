ec6103c9c4e9fdee081b356773ade2e2d4723605
Jean-Paul Laberge <jean-paul.laberge@bell.ca>
2016-06-16 13:48:55 -0400

    changes for mitaka

--- dockerfiles/nova/setup.sh
+++ dockerfiles/nova/setup.sh
@@ -7,8 +7,8 @@ then
 fi
 
 cat <<EOF > /admin-openrc.sh
-export OS_PROJECT_DOMAIN_ID=default
-export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
 export OS_PROJECT_NAME=admin
 export OS_TENANT_NAME=admin
 export OS_USERNAME=admin
@@ -19,8 +19,8 @@ export OS_IMAGE_API_VERSION=2
 EOF
 
 cat <<EOF > /demo-openrc.sh
-export OS_PROJECT_DOMAIN_ID=default
-export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
 export OS_PROJECT_NAME=demo
 export OS_TENANT_NAME=demo
 export OS_USERNAME=demo
@@ -34,13 +34,17 @@ EOF
 source /admin-openrc.sh
 
 
-echo "CREATE DATABASE nova;
+echo "CREATE DATABASE nova_api;
+GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'localhost' \
+  IDENTIFIED BY '$NOVA_DBPASS';
+GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'%' \
+  IDENTIFIED BY '$NOVA_DBPASS';
+CREATE DATABASE nova;
 GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' \
-  IDENTIFIED BY '$NOVA_DBPASS';
-GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' \
-  IDENTIFIED BY '$NOVA_DBPASS';
+    IDENTIFIED BY '$NOVA_DBPASS';
+  GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' \
+    IDENTIFIED BY '$NOVA_DBPASS';
 FLUSH PRIVILEGES;" | mysql --user=root --password=$MYSQL_ROOT_PASSWORD -h $MYSQLHOST -P 3306
-
 
 
 ./wait_for_ks_admin_ep.sh
@@ -52,13 +56,17 @@ openstack role add --project service --user nova admin
 openstack service create --name nova \
   --description "OpenStack Compute" compute
 openstack endpoint create --region $REGION1 \
-  compute public http://$NOVA_HOST:8774/v2/%\(tenant_id\)s
+  compute public http://$NOVA_HOST:8774/v2.1/%\(tenant_id\)s
 openstack endpoint create --region $REGION1 \
-  compute internal http://$NOVA_HOST:8774/v2/%\(tenant_id\)s
+  compute internal http://$NOVA_HOST:8774/v2.1/%\(tenant_id\)s
 openstack endpoint create --region $REGION1 \
-  compute admin http://$NOVA_HOST:8774/v2/%\(tenant_id\)s  
+  compute admin http://$NOVA_HOST:8774/v2.1/%\(tenant_id\)s
 
 cp /etc/nova/nova.conf /etc/nova/nova.conf.bak
+crudini --set /etc/nova/nova.conf DEFAULT enabled_apis osapi_compute,metadata
+
+crudini --set /etc/nova/nova.conf api_database connection mysql+pymysql://nova:$NOVA_DBPASS@$MYSQLHOST/nova_api
+
 crudini --set /etc/nova/nova.conf database connection mysql+pymysql://nova:$NOVA_DBPASS@$MYSQLHOST/nova
 
 crudini --set /etc/nova/nova.conf DEFAULT rpc_backend rabbit
@@ -69,51 +77,53 @@ crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_password $RABBITM
 crudini --set /etc/nova/nova.conf DEFAULT auth_strategy keystone
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_uri http://$KEYSTONE_HOST:5000
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_url http://$KEYSTONE_HOST:35357
-crudini --set /etc/nova/nova.conf keystone_authtoken auth_plugin password
-crudini --set /etc/nova/nova.conf keystone_authtoken project_domain_id default 
-crudini --set /etc/nova/nova.conf keystone_authtoken user_domain_id default
+crudini --set /etc/nova/nova.conf keystone_authtoken memcached_servers $KEYSTONE_HOST:11211
+crudini --set /etc/nova/nova.conf keystone_authtoken auth_type password
+crudini --set /etc/nova/nova.conf keystone_authtoken project_domain_name default
+crudini --set /etc/nova/nova.conf keystone_authtoken user_domain_name default
 crudini --set /etc/nova/nova.conf keystone_authtoken project_name service
 crudini --set /etc/nova/nova.conf keystone_authtoken username nova
 crudini --set /etc/nova/nova.conf keystone_authtoken password $NOVA_PASS
 
 crudini --set /etc/nova/nova.conf DEFAULT my_ip $NOVA_HOST
-crudini --set /etc/nova/nova.conf DEFAULT network_api_class nova.network.neutronv2.api.API
-crudini --set /etc/nova/nova.conf DEFAULT security_group_api neutron
-crudini --set /etc/nova/nova.conf DEFAULT linuxnet_interface_driver nova.network.linux_net.LinuxOVSInterfaceDriver
+#crudini --set /etc/nova/nova.conf DEFAULT network_api_class nova.network.neutronv2.api.API
+#crudini --set /etc/nova/nova.conf DEFAULT security_group_api neutron
+#crudini --set /etc/nova/nova.conf DEFAULT linuxnet_interface_driver nova.network.linux_net.LinuxOVSInterfaceDriver
+crudini --set /etc/nova/nova.conf DEFAULT use_neutron True
 crudini --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
 
 crudini --set /etc/nova/nova.conf vnc vncserver_listen $NOVA_HOST
 crudini --set /etc/nova/nova.conf vnc vncserver_proxyclient_address $NOVA_HOST
 
-crudini --set /etc/nova/nova.conf glance host $GLANCE_HOST
+#crudini --set /etc/nova/nova.conf glance host $GLANCE_HOST
+crudini --set /etc/nova/nova.conf glance api_servers http://$GLANCE_HOST:9292
 
 crudini --set /etc/nova/nova.conf oslo_concurrency lock_path /var/lib/nova/tmp
-
-crudini --set /etc/nova/nova.conf DEFAULT enabled_apis osapi_compute,metadata
 
 crudini --set /etc/nova/nova.conf DEFAULT verbose True
 
 # NETRON serup
-crudini --set /etc/nova/nova.conf neutron url http://$NEUTRON_HOST:9696
-crudini --set /etc/nova/nova.conf neutron auth_url http://$KEYSTONE_HOST:35357
-crudini --set /etc/nova/nova.conf neutron auth_plugin password
-crudini --set /etc/nova/nova.conf neutron project_domain_id default
-crudini --set /etc/nova/nova.conf neutron user_domain_id default
-crudini --set /etc/nova/nova.conf neutron region_name $REGION1
-crudini --set /etc/nova/nova.conf neutron project_name service
-crudini --set /etc/nova/nova.conf neutron username neutron
-crudini --set /etc/nova/nova.conf neutron password $NEUTRON_PASS
-crudini --set /etc/nova/nova.conf neutron service_metadata_proxy True
-crudini --set /etc/nova/nova.conf neutron metadata_proxy_shared_secret $METADATA_SECRET
+#crudini --set /etc/nova/nova.conf neutron url http://$NEUTRON_HOST:9696
+#crudini --set /etc/nova/nova.conf neutron auth_url http://$KEYSTONE_HOST:35357
+#crudini --set /etc/nova/nova.conf neutron auth_plugin password
+#crudini --set /etc/nova/nova.conf neutron project_domain_id default
+#crudini --set /etc/nova/nova.conf neutron user_domain_id default
+#crudini --set /etc/nova/nova.conf neutron region_name $REGION1
+#crudini --set /etc/nova/nova.conf neutron project_name service
+#crudini --set /etc/nova/nova.conf neutron username neutron
+#crudini --set /etc/nova/nova.conf neutron password $NEUTRON_PASS
+#crudini --set /etc/nova/nova.conf neutron service_metadata_proxy True
+#crudini --set /etc/nova/nova.conf neutron metadata_proxy_shared_secret $METADATA_SECRET
 
 diff /etc/nova/nova.conf /etc/nova/nova.conf.bak
 
 sleep 5
 
-su -s /bin/sh -c "nova-manage db sync" nova 2>&1 > /var/log/nova/nova-manage.out 
+su -s /bin/sh -c "nova-manage api_db sync" nova
+su -s /bin/sh -c "nova-manage db sync" nova
 
 service nova-api restart
-service nova-cert restart
+#service nova-cert restart
 service nova-consoleauth restart
 service nova-scheduler restart
 service nova-conductor restart
