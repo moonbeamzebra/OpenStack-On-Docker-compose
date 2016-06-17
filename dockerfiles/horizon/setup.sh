40350cc8abf1e6bf841a08bdb596d560543607da
Jean-Paul Laberge <jean-paul.laberge@bell.ca>
2016-06-17 09:52:12 -0400

    fix authentiaction

--- dockerfiles/horizon/setup.sh
+++ dockerfiles/horizon/setup.sh
@@ -31,7 +31,22 @@ export OS_IMAGE_API_VERSION=2
 EOF
 
 
-#sed -i.bak "s/ALLOWED_HOSTS = \['horizon.example.com', 'localhost'\]/ALLOWED_HOSTS = ['*']/" /etc/openstack-dashboard/local_settings.py
-sed -i 's/OPENSTACK_HOST = "127.0.0.1"/OPENSTACK_HOST = "'"$KEYSTONE_HOST"'"/' /etc/openstack-dashboard/local_settings.py
+sed -i.bak 's/OPENSTACK_HOST = "127.0.0.1"/OPENSTACK_HOST = "'"$KEYSTONE_HOST"'"/' /etc/openstack-dashboard/local_settings.py
+
+sed -i 's/v2.0"/v3"/' /etc/openstack-dashboard/local_settings.py
+
+sed -i 's/OPENSTACK_KEYSTONE_DEFAULT_ROLE = "_member_"/OPENSTACK_KEYSTONE_DEFAULT_ROLE = "user"/' /etc/openstack-dashboard/local_settings.py
+
+
+#sed -i 's/#OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = False/OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = True/' /etc/openstack-dashboard/local_settings.py
+echo 'OPENSTACK_API_VERSIONS = {
+    "identity": 3,
+    "image": 2,
+    "volume": 2,
+}' >> /etc/openstack-dashboard/local_settings.py
+
+echo 'OPENSTACK_KEYSTONE_DEFAULT_DOMAIN = "default"' >> /etc/openstack-dashboard/local_settings.py
+
+
 
 touch /setup.done
