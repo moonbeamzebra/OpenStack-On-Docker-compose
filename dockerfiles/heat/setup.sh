74f2030c7656eef2a1a1b5f12f2c6214149487e6
jplaberge <moonbeamzebra@magenta.ca>
2016-06-18 07:01:42 -0400

    add mitaka support

--- dockerfiles/heat/setup.sh
+++ dockerfiles/heat/setup.sh
@@ -7,8 +7,8 @@ then
 fi
 
 cat <<EOF > /admin-openrc.sh
-export OS_PROJECT_DOMAIN_ID=default
-export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
 export OS_PROJECT_NAME=admin
 export OS_TENANT_NAME=admin
 export OS_USERNAME=admin
@@ -19,8 +19,8 @@ export OS_IMAGE_API_VERSION=2
 EOF
 
 cat <<EOF > /demo-openrc.sh
-export OS_PROJECT_DOMAIN_ID=default
-export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
 export OS_PROJECT_NAME=demo
 export OS_TENANT_NAME=demo
 export OS_USERNAME=demo
@@ -33,7 +33,8 @@ EOF
 
 source /admin-openrc.sh
 
-echo "CREATE DATABASE heat;
+echo "DROP DATABASE IF EXISTS heat;
+CREATE DATABASE heat;
 GRANT ALL PRIVILEGES ON heat.* TO 'heat'@'localhost' IDENTIFIED BY '$HEAT_DBPASS';
 GRANT ALL PRIVILEGES ON heat.* TO 'heat'@'%' IDENTIFIED BY '$HEAT_DBPASS';
 FLUSH PRIVILEGES;" | mysql --user=root --password=$MYSQL_ROOT_PASSWORD -h $MYSQLHOST -P 3306
@@ -62,6 +63,7 @@ openstack endpoint create --region $REGION1 \
   cloudformation internal http://$HEAT_HOST:8000/v1
 openstack endpoint create --region $REGION1 \
   cloudformation admin http://$HEAT_HOST:8000/v1
+
 openstack domain create --description "Stack projects and users" heat
 openstack user create --domain heat --password $HEAT_DOMAIN_PASS heat_domain_admin
 openstack role add --domain heat --user heat_domain_admin admin
@@ -75,15 +77,16 @@ cp /etc/heat/heat.conf /etc/heat/heat.conf.bak
 crudini --set /etc/heat/heat.conf database connection mysql+pymysql://heat:$HEAT_DBPASS@$MYSQLHOST/heat
 
 crudini --set /etc/heat/heat.conf DEFAULT rpc_backend rabbit
-crudini --set /etc/heat/heat.conf  oslo_messaging_rabbit rabbit_host $RABBIT_HOST
+crudini --set /etc/heat/heat.conf oslo_messaging_rabbit rabbit_host $RABBIT_HOST
 crudini --set /etc/heat/heat.conf oslo_messaging_rabbit rabbit_userid $RABBITMQ_DEFAULT_USER
 crudini --set /etc/heat/heat.conf oslo_messaging_rabbit rabbit_password $RABBITMQ_DEFAULT_PASS
 
 crudini --set /etc/heat/heat.conf keystone_authtoken auth_uri http://$KEYSTONE_HOST:5000
 crudini --set /etc/heat/heat.conf keystone_authtoken auth_url http://$KEYSTONE_HOST:35357
-crudini --set /etc/heat/heat.conf keystone_authtoken auth_plugin password
-crudini --set /etc/heat/heat.conf keystone_authtoken project_domain_id default
-crudini --set /etc/heat/heat.conf keystone_authtoken user_domain_id default
+crudini --set /etc/heat/heat.conf keystone_authtoken memcached_servers $KEYSTONE_HOST:11211
+crudini --set /etc/heat/heat.conf keystone_authtoken auth_type password
+crudini --set /etc/heat/heat.conf keystone_authtoken project_domain_name default
+crudini --set /etc/heat/heat.conf keystone_authtoken user_domain_name default
 crudini --set /etc/heat/heat.conf keystone_authtoken project_name service
 crudini --set /etc/heat/heat.conf keystone_authtoken username heat
 crudini --set /etc/heat/heat.conf keystone_authtoken password $HEAT_PASS
@@ -91,8 +94,8 @@ crudini --set /etc/heat/heat.conf keystone_authtoken password $HEAT_PASS
 crudini --set /etc/heat/heat.conf trustee auth_uri http://$KEYSTONE_HOST:5000
 crudini --set /etc/heat/heat.conf trustee auth_url http://$KEYSTONE_HOST:35357
 crudini --set /etc/heat/heat.conf trustee auth_plugin password
-crudini --set /etc/heat/heat.conf trustee project_domain_id default
-crudini --set /etc/heat/heat.conf trustee user_domain_id default
+crudini --set /etc/heat/heat.conf trustee project_domain_name default
+crudini --set /etc/heat/heat.conf trustee user_domain_name default
 crudini --set /etc/heat/heat.conf trustee project_name service
 crudini --set /etc/heat/heat.conf trustee username heat
 crudini --set /etc/heat/heat.conf trustee password $HEAT_PASS
