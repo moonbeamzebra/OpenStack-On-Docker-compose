8744eab65288d2765296151b92f08f48de8ed454
Jean-Paul Laberge <jean-paul.laberge@bell.ca>
2016-06-20 11:51:48 -0400

    finish adding mitaka support

--- net-and-cmp-scripts/d_ovs_compute_mitaka_Self-service_networks.sh
+++ net-and-cmp-scripts/d_ovs_compute_mitaka_Self-service_networks.sh
@@ -18,21 +18,21 @@ HEAT_PASS=heat1
 CEIL_PASS=lab1
 DEMO_PASS=demo
 METADATA_SECRET=metadata_secret1
-RABBIT_HOST=10.199.1.220
-KEYSTONE_HOST=10.199.1.220
-HORIZON_HOST=10.199.1.220
-CINDER_HOST=10.199.1.220
-GLANCE_HOST=10.199.1.220
-NOVA_HOST=10.199.1.220
-NEUTRON_HOST=10.199.1.220
-HEAT_HOST=10.199.1.220
-MONGO_HOST=10.199.1.220
-CEIL_HOST=10.199.1.220
-MYSQLHOST=10.199.1.220
+RABBIT_HOST=10.10.10.10
+KEYSTONE_HOST=10.10.10.10
+HORIZON_HOST=10.10.10.10
+CINDER_HOST=10.10.10.10
+GLANCE_HOST=10.10.10.10
+NOVA_HOST=10.10.10.10
+NEUTRON_HOST=10.10.10.10
+HEAT_HOST=10.10.10.10
+MONGO_HOST=10.10.10.10
+CEIL_HOST=10.10.10.10
+MYSQLHOST=10.10.10.10
 REGION1=RegionOne
-CMP1_MANAGEMENT_INTERFACE_IP_ADDRESS=10.199.1.222
-NET_OVERLAY_INTERFACE_IP_ADDRESS=10.199.5.221
-CMP1_OVERLAY_INTERFACE_IP_ADDRESS=10.199.5.222
+CMP1_MANAGEMENT_INTERFACE_IP_ADDRESS=10.10.10.12
+NET_OVERLAY_INTERFACE_IP_ADDRESS=10.10.11.11
+CMP1_OVERLAY_INTERFACE_IP_ADDRESS=10.10.11.12
 NET_PUBLIC_INTERFACE_NAME=eth2
 CMP1_PUBLIC_INTERFACE_NAME=eth2
 
@@ -54,24 +54,11 @@ apt-get install -y \
 
 cp /etc/nova/nova.conf /etc/nova/nova.conf.bak
 crudini --set /etc/nova/nova.conf DEFAULT rpc_backend rabbit
-crudini --set /etc/nova/nova.conf DEFAULT auth_strategy keystone
-crudini --set /etc/nova/nova.conf DEFAULT my_ip $CMP1_MANAGEMENT_INTERFACE_IP_ADDRESS
-crudini --set /etc/nova/nova.conf DEFAULT network_api_class nova.network.neutronv2.api.API
-crudini --set /etc/nova/nova.conf DEFAULT security_group_api neutron
-crudini --set /etc/nova/nova.conf DEFAULT linuxnet_interface_driver nova.network.linux_net.LinuxOVSInterfaceDriver
-crudini --set /etc/nova/nova.conf DEFAULT use_neutron True
-crudini --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
-crudini --set /etc/nova/nova.conf DEFAULT verbose True
-
-crudini --set /etc/nova/nova.conf DEFAULT instance_usage_audit True
-crudini --set /etc/nova/nova.conf DEFAULT instance_usage_audit_period hour
-crudini --set /etc/nova/nova.conf DEFAULT notify_on_state_change vm_and_task_state
-crudini --set /etc/nova/nova.conf DEFAULT notification_driver messagingv2
-
 crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_host $RABBIT_HOST
 crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_userid $RABBITMQ_DEFAULT_USER
 crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_password $RABBITMQ_DEFAULT_PASS
 
+crudini --set /etc/nova/nova.conf DEFAULT auth_strategy keystone
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_uri http://$KEYSTONE_HOST:5000
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_url http://$KEYSTONE_HOST:35357
 crudini --set /etc/nova/nova.conf keystone_authtoken memcached_servers $KEYSTONE_HOST:11211
@@ -81,6 +68,12 @@ crudini --set /etc/nova/nova.conf keystone_authtoken user_domain_name default
 crudini --set /etc/nova/nova.conf keystone_authtoken project_name service
 crudini --set /etc/nova/nova.conf keystone_authtoken username nova
 crudini --set /etc/nova/nova.conf keystone_authtoken password $NOVA_PASS
+
+crudini --set /etc/nova/nova.conf DEFAULT my_ip $CMP1_MANAGEMENT_INTERFACE_IP_ADDRESS
+
+crudini --set /etc/nova/nova.conf DEFAULT use_neutron True
+crudini --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
+crudini --set /etc/nova/nova.conf DEFAULT verbose True
 
 crudini --set /etc/nova/nova.conf vnc enabled True
 crudini --set /etc/nova/nova.conf vnc vncserver_listen 0.0.0.0
@@ -93,16 +86,29 @@ crudini --set /etc/nova/nova.conf oslo_concurrency lock_path /var/lib/nova/tmp
 
 crudini --set /etc/nova/nova.conf neutron url http://$NEUTRON_HOST:9696
 crudini --set /etc/nova/nova.conf neutron auth_url http://$KEYSTONE_HOST:35357
-crudini --set /etc/nova/nova.conf neutron auth_plugin password
-crudini --set /etc/nova/nova.conf neutron project_domain_id default
-crudini --set /etc/nova/nova.conf neutron user_domain_id default
+crudini --set /etc/nova/nova.conf neutron auth_type password
+crudini --set /etc/nova/nova.conf neutron project_domain_name default
+crudini --set /etc/nova/nova.conf neutron user_domain_name default
 crudini --set /etc/nova/nova.conf neutron region_name $REGION1
 crudini --set /etc/nova/nova.conf neutron project_name service
 crudini --set /etc/nova/nova.conf neutron username neutron
 crudini --set /etc/nova/nova.conf neutron password $NEUTRON_PASS
 
+
+# Following 3 not documented as such
+crudini --set /etc/nova/nova.conf DEFAULT network_api_class nova.network.neutronv2.api.API
+crudini --set /etc/nova/nova.conf DEFAULT security_group_api neutron
+crudini --set /etc/nova/nova.conf DEFAULT linuxnet_interface_driver nova.network.linux_net.LinuxOVSInterfaceDriver
+
+# Next for ceilometer
+crudini --set /etc/nova/nova.conf DEFAULT instance_usage_audit True
+crudini --set /etc/nova/nova.conf DEFAULT instance_usage_audit_period hour
+crudini --set /etc/nova/nova.conf DEFAULT notify_on_state_change vm_and_task_state
+crudini --set /etc/nova/nova.conf DEFAULT notification_driver messagingv2
+
 diff /etc/nova/nova.conf /etc/nova/nova.conf.bak
 
+# Next for ceilometer
 cp /etc/ceilometer/ceilometer.conf /etc/ceilometer/ceilometer.conf.bak
 crudini --set /etc/ceilometer/ceilometer.conf DEFAULT rpc_backend rabbit
 crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_host $RABBIT_HOST
