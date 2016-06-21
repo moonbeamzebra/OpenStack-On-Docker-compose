dea4ae7d59235a8985508d8bf50113154927306a
jplaberge <moonbeamzebra@magenta.ca>
2016-06-20 21:33:03 -0400

    use neutron-openvswitch-agent name for service

--- net-and-cmp-scripts/d_ovs_network_mitaka_Self-service_networks.sh
+++ net-and-cmp-scripts/d_ovs_network_mitaka_Self-service_networks.sh
@@ -179,7 +179,7 @@ service openvswitch-switch restart
 ovs-vsctl add-br br-ex
 ovs-vsctl add-port br-ex $NET_PUBLIC_INTERFACE_NAME
 
-service neutron-plugin-openvswitch-agent restart
+service neutron-openvswitch-agent restart
 service neutron-l3-agent restart
 service neutron-dhcp-agent restart
 service neutron-metadata-agent restart
